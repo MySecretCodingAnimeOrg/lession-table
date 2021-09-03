52a816d5320f9a832da10f1524637d24e202b827
ArcheryFox <archeryfox2005@gmail.com>
2021-09-03 23:45:30 +0300

    days

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function daty() {
   var thema = document.getElementsByClassName('time')[0];
   thema.innerHTML = '<b>Сегодня </b> <br />' + date.getFullYear() + '<br />' + date.getDate() + '-e ' + month[date.getMonth()] + ', ' + weeks[date.getDay()] + ". " + '<br />' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
 }
+if(date.getDay()==0){
+  document.getElementById('monday').style.border = 'border: #e34636 outset 10px'
+}
+if(date.getDay()==1){
+  document.getElementById('tuesday').style.border = 'border: #e34636 outset 10px'
+}
+if(date.getDay()==2){
+  document.getElementById('wensday').style.border = 'border: #e34636 outset 10px'
+}
+if(date.getDay()==3){
+  document.getElementById('thursday').style.border = 'border: #e34636 outset 10px'
+}
+if(date.getDay()==4){
+  document.getElementById('friday').style.border = 'border: #e34636 outset 10px'
+}
+if(date.getDay()==5){
+  document.getElementById('saturday').style.border = 'border: #e34636 outset 10px'
+}
