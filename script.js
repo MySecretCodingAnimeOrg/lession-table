8f08123b6ca0bbd4121e705b69db614629e6583c
ArcheryFox <archeryfox2005@gmail.com>
2021-09-04 23:28:00 +0300

    Update script.js
    Co-Authored-By: AliveSaiTeg <89860140+AliveSaiTeg@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-var test = setInterval(daty, 100)
-var date = new Date();
-month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
-weeks = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
-
-function daty() {
-  let date = new Date();
-  var thema = document.getElementsByClassName('time')[0];
-  thema.innerHTML = '<b>Сегодня </b> <br />' + date.getFullYear() + '<br />' + date.getDate() + '-e ' + month[date.getMonth()] + ', ' + weeks[date.getDay()] + ". " + '<br />' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-  
-if(date.getHours()>5&&date.getHours()<16){
-  document.body.style.backgroundImage = "url('lamp_vlight.png')";
-} else {
-  document.body.style.backgroundImage = "url('lamp_v3.png')";
-}
-
-}
+// var test = setInterval(daty, 1000)
+// var date = new Date();
+// month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+// weeks = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+// function daty() {
+//   let date = new Date();
+//   var thema = document.getElementsByClassName('time')[0];
+//   thema.innerHTML = '<b>Сегодня </b> <br />' + date.getFullYear() + '<br />' + date.getDate() + '-e ' + month[date.getMonth()] + ', ' + weeks[date.getDay()] + ". " + '<br />' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+// if(date.getHours()>5&&date.getHours()<16){
+//   document.body.style.backgroundImage = "url('lamp_vlight.png')";
+// } else {
+//   document.body.style.backgroundImage = "url('lamp_v3.png')";
+// }
+// }
